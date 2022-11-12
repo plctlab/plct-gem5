3db9ddb6c33a2ca0b0d25abddb43fab78d546730
Hoa Nguyen <hoanguyen@ucdavis.edu>
2022-11-12 03:54:04 +0000

    arch-riscv: Remove a change that added a blank line
    Change-Id: I14172a4cf517f3f81d9cf3928c7db32f49644b20
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/arch/riscv/fp_inst.hh
+++ src/arch/riscv/fp_inst.hh
@@ -40,5 +40,4 @@
             return std::make_shared<IllegalInstFault>("RM fault", machInst);\
         softfloat_roundingMode = rm;                                        \
 
-
 #endif // __ARCH_RISCV_FP_INST_HH__
